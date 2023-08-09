37b6be48b7182b3b0713faaa4d7518e26ef0e952
shaberry <jaden.png02@gmail.com>
2023-08-09 14:32:41 -0700

    button

--- React-Forms/src/components/Authenticate.jsx
+++ React-Forms/src/components/Authenticate.jsx
@@ -1,13 +1,36 @@
 // import SignupForm from "./SignUpForm";
 // import Authenticate from "./Authenticate";
 
+import { useState } from "react";
+
+
 export default function Authenticate(props){
+   const [error,setError] = useState(null);
+
   const  {token, setToken} = props;
-// asyn function handleClick()
+  
+async function handleClick(){
+   try{
+      const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup",
+      {
+         method: "GET",
+         headers: {"Content-Type" : "applicatoin/json", Authorization: `Bearer ${token}`
+         }
+      } 
+   );
+   const result = await response.json();
+   }catch(error){
+      setError(error.message)
+   }
+   console.log(handleClick)
+}
+
+
    return (
    <>
    <h2>Authenticate</h2>
-   <button onClick={token}>Authenticate Token</button>
+   { error && <p>{error}</p>}
+   <button onClick={handleClick}>Authenticate Token</button>
    </>
    )
 }
